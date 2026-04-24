8f4653202dfdb6755ca847738cb05ffb31149d53
Antzelook <akermezo@mes-digital.com>
2026-04-24 12:24:40 +0300

    move types from error form

--- types/index.ts
+++ types/index.ts
@@ -19,5 +19,23 @@ export type UserSignInForm = {
   password: string;
 };
 
+export const typeOptions: string[] = [
+  "Boot Loop",
+  "Motor Error",
+  "Low Battery",
+  "Διαρροή",
+  "Πόρτα",
+  "'Άλλο",
+];
+
+export const actionOptions: string[] = [
+  "Flash Firmware",
+  "Callibrate Valve",
+  "Αλλαγή Μπαταρίας",
+  "Επισκευή Διαρροής",
+  "Κλείσιμο Πόρτας",
+  "Άλλο",
+];
+
 export type ErrorFormType = z.infer<typeof errorFormSchema>;
 export type UserSignInFormType = z.infer<typeof userSignInSchema>;
